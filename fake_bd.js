43944272b86c9e4a75e7d57e196806bc914e2513
Akarmain <101408699+akarmain@users.noreply.github.com>
2024-05-14 10:02:24 +0300

    added sort and python

---
+++ fake_bd.js
@@ -0,0 +1,4 @@
+   module.exports.get_data = function(moduleId) {
+        return moduleId
+
+   };
--- main.js
+++ main.js
@@ -1,32 +1,41 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const {get_data} = require("./fake_bd.js");
 
 app.use(express.static(path.join(__dirname, 'views')))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', function(req, res) {
-  res.render('index');
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
-app.get('/main/:moduleId', function(req, res) {
-  var moduleId = req.params.moduleId.toUpperCase();
-  if (['M1', "M2", 'M3', "M4", "M5"].includes(moduleId)) {
-    res.render(`pages/modules/${moduleId.toLowerCase()}.ejs`);
-  } else {
-    const filePath = path.join(__dirname, 'views/pages/template/code_error_404.html');
-    res.status(404).sendFile(filePath);
-  }
+app.get('/main/:moduleId', function (req, res) {
+    var moduleId = req.params.moduleId.toUpperCase();
+    if (['M1', "M2", 'M3', "M4", "M5", "SORT"].includes(moduleId)) {
+
+
+        const data = {
+            data: get_data(moduleId)
+        };
+
+        console.log(data);
+        res.render(`pages/modules/${moduleId.toLowerCase()}.ejs`, data);
+
+    } else {
+        const filePath = path.join(__dirname, 'views/pages/template/code_error_404.html');
+        res.status(404).sendFile(filePath);
+    }
 });
 
 app.use((req, res, next) => {
-  const filePath = path.join(__dirname, 'views/pages/template/code_error_404.html');
-  res.status(404).sendFile(filePath);
+    const filePath = path.join(__dirname, 'views/pages/template/code_error_404.html');
+    res.status(404).sendFile(filePath);
 });
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3004
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
